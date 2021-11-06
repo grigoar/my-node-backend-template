b9d92d5d9e166f2d850b17a0b644c1a13fdcbf7b
grigoar <grigore.nath.j@gmail.com>
2021-11-06 12:31:09 +0200

    added JWT token for signup and login

--- controller/authController.js
+++ controller/authController.js
@@ -1,14 +1,49 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-body-style */
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+//Create the JWT token
+const signToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: { user },
+  });
+};
+
 exports.signup = catchAsync(async (req, res) => {
   // console.log(req.params.id);
-  const newUser = await User.create(req.body);
-  res.status(200).json({
-    status: 'success',
-    data: newUser,
+  //   const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email.toLowerCase(),
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    //the role or any other resources should be changed securely after
+    // role: req.body.role
   });
+
+  createSendToken(newUser, 201, res);
+  //   const token = signToken(newUser._id);
+  //   newUser.password = undefined;
+  //   res.status(200).json({
+  //     status: 'success',
+  //     token,
+  //     data: newUser,
+  //   });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -20,13 +55,16 @@ exports.login = catchAsync(async (req, res, next) => {
 
   const user = await User.findOne({ email }).select('+password');
 
-  const correct = await user.correctPassword(password, user.password);
-  if (!correct) {
+  //   const correct = await user.correctPassword(password, user.password);
+  if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password!', 401));
   }
-
-  res.status(200).json({
-    status: 'success',
-    data: user,
-  });
+  //3)If everything is correct send the token to client
+  createSendToken(user, 200, res);
+  //   const token = signToken(user._id);
+  //   res.status(200).json({
+  //     status: 'success',
+  //     token,
+  //     // data: user,
+  //   });
 });
